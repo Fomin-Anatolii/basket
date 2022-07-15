3d13ff81a0e1059469fda851bebc444b3f671996
Anatoly Fomin <foma.laoshi@gmail.com>
2022-07-15 15:31:01 +0800

    modified Increment/Decrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react"
-
 const Counter = (props) => {
-  const [value, setValue] = useState(props.value)
+  const { value } = props
 
   const formatValue = () => {
     return value === 0 ? "empty" : value
@@ -13,22 +11,14 @@ const Counter = (props) => {
     return classes
   }
 
-  const handleIncrement = () => {
-    setValue((prevValue) => prevValue + 1)
-  }
-
-  const handleDecrement = () => {
-    setValue((prevValue) => prevValue - 1)
-  }
-
   return (
     <div>
       <span>{props.name}</span>
       <span className={getBadgeClasses()}>{formatValue()}</span>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+      <button className="btn btn-primary btn-sm m-2" onClick={() => props.onIncrement(props.id)}>
         +
       </button>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+      <button className="btn btn-primary btn-sm m-2" onClick={() => props.onDecrement(props.id)}>
         -
       </button>
       <button className="btn btn-danger btn-sm m-2" onClick={() => props.onDelete(props.id)}>
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,24 +2,47 @@ import { useState } from "react"
 import Counter from "./counter"
 
 const CountersList = () => {
-  const [counters, setCounters] = useState([
+  const initialState = [
     { id: 0, value: 0, name: `Ненужная вещь` },
     { id: 1, value: 4, name: `Ложка` },
     { id: 2, value: 0, name: `Вилка` },
     { id: 3, value: 0, name: `Тарелка` },
     { id: 4, value: 0, name: `Набор минималиста` },
-  ])
+  ]
+  const [counters, setCounters] = useState(initialState)
 
   const handleDelete = (id) => {
-    console.log(`handleDelete`, id)
     const newCounters = counters.filter((c) => c.id !== id)
     setCounters(newCounters)
   }
+
+  const handleReset = () => {
+    setCounters(initialState)
+  }
+
+  const handleIncrement = (value) => {
+    setCounters(counters.map((c) => c.value === value++))
+  }
+
+  const handleDecrement = (value) => {
+    let newDecrement = counters.map((c) => c.value === value--)
+    setCounters(newDecrement)
+  }
+
   return (
     <>
       {counters.map((count) => (
-        <Counter key={count.id} {...count} onDelete={handleDelete} />
+        <Counter
+          key={count.id}
+          {...count}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+        />
       ))}
+      <button className="btn btn-danger btn-sm m-2" onClick={handleReset}>
+        Reset
+      </button>
     </>
   )
 }
